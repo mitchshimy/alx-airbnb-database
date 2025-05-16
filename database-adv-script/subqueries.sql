fb7cfc09437ab60ebb5086d9d8d55a21e83dee64
ShimY <42277453+mitchshimy@users.noreply.github.com>
2025-05-16 12:26:32 +0300

    Add files via upload

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,22 @@
+
+--Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
+
+SELECT properties.name AS Property_name
+FROM properties
+wHERE property_id IN(
+    SELECT property_id
+    FROM reviews
+    GROUP BY  property_id
+    HAVING AVG(rating) > 4.0
+);
+
+--Write a correlated subquery to find users who have made more than 3 bookings.
+
+SELECT u.first_name, u.last_name
+FROM user u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
+
