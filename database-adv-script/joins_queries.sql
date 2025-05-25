80f38db6a8ea4332134498508434665602850269
ShimY <42277453+mitchshimy@users.noreply.github.com>
2025-05-25 09:15:15 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,3 +7,30 @@ SELECT
 FROM bookings b
 INNER JOIN user u ON b.user_id = u.user_id
 INNER JOIN properties p ON b.property_id = p.property_id;
+
+SELECT 
+    b.booking_id,
+    b.created_at,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.first_name,
+    u.last_name
+FROM users u
+RIGHT JOIN bookings b ON u.user_id = b.user_id;
