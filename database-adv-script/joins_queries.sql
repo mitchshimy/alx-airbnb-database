e92d84ca841858bb86a360ee6c19c610f1ebe2bb
ShimY <42277453+mitchshimy@users.noreply.github.com>
2025-05-25 09:18:20 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,8 @@ SELECT
   p.name AS property_name
 FROM bookings b
 INNER JOIN user u ON b.user_id = u.user_id
-INNER JOIN properties p ON b.property_id = p.property_id;
+INNER JOIN properties p ON b.property_id = p.property_id
+ORDER BY b.booking_id ASC;
 
 SELECT 
     b.booking_id,
@@ -23,7 +24,8 @@ SELECT
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ORDER BY b.created_at DESC;
 
 SELECT 
     b.booking_id,
@@ -33,4 +35,18 @@ SELECT
     u.first_name,
     u.last_name
 FROM users u
-RIGHT JOIN bookings b ON u.user_id = b.user_id;
+RIGHT JOIN bookings b ON u.user_id = b.user_id
+ORDER BY b.start_date ASC;
+
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.start_date,
+    b.end_date
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
+
